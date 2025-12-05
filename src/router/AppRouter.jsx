903eba8515fe2b3019346f7db34507eaddcad7ba
kancharla prudhvi <kancharlaprudhvi2002@gmail.com>
2025-12-05 15:55:19 +0530

    Teacher Dashboard updated

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,7 +5,7 @@ import Auth from "../pages/auth/Auth.jsx";
 import AdminDashboard from "../pages/admin/AdminDashboard.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 import StudentDashboard from "../pages/student/StudentDashboard.jsx";
-
+import TeacherDashboard from "../pages/teacher/TeacherDashboard.jsx";
 export default function AppRouter() {
   return (
     <>
@@ -18,6 +18,7 @@ export default function AppRouter() {
             <Route path="/auth" element={<Auth />} />
             <Route path="/student" element={<StudentDashboard />} />
             <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            <Route path="/teacher" element={<TeacherDashboard />} />
           </Routes>
         </div>
 
